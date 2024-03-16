7e02eb0121e0d6b5547537ca36e8dc24ab4a56fc
Thobela Sixpence <61964782+thobs-10@users.noreply.github.com>
2024-03-16 22:55:29 +0200

    Added files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'src',
+    version= '0.0.0',
+    author= 'Thobela Theophilus Sixpence',
+    author_email= 'thobela.sixpence@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
